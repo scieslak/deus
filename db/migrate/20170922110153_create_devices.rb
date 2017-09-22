6352ae10e1c528c97dd6fc8f6321ce5d6f542ce4
scieslak <scieslak@scieslak.com>
2017-09-22 12:10:42 +0100

    Generate Device scaffold

---
+++ db/migrate/20170922110153_create_devices.rb
@@ -0,0 +1,10 @@
+class CreateDevices < ActiveRecord::Migration[5.1]
+  def change
+    create_table :devices do |t|
+      t.string :name
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Device.create name: "ESP8266 1", status: 200
+Device.create name: "ESP8266 2", status: 500
